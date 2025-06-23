c3bfb08f71a7a189dc2ee986bf182c4451f9427c
hs <96301516+tmkayy@users.noreply.github.com>
2025-06-23 20:47:15 +0300

    added error handling

--- MonOOPoly/Source.cpp
+++ MonOOPoly/Source.cpp
@@ -3,40 +3,46 @@
 #include <iostream>
 
 int main() {
-    Monopoly game;
-    while (true) {
-        std::cout << "==== Monopoly ====" << std::endl;
-        std::cout << "1. New Game" << std::endl;
-        std::cout << "2. Load Game" << std::endl;
-        std::cout << "3. Exit" << std::endl;
-        std::cout << "Select option (1-3): ";
-        int choice;
-        std::cin >> choice;
+    try {
+        Monopoly game;
+        while (true) {
+            std::cout << "==== Monopoly ====" << std::endl;
+            std::cout << "1. New Game" << std::endl;
+            std::cout << "2. Load Game" << std::endl;
+            std::cout << "3. Exit" << std::endl;
+            std::cout << "Select option (1-3): ";
+            int choice;
+            std::cin >> choice;
 
-        if (choice == 1) {
-			system("cls");
-            game.initializePlayers();
-            game.startGame();
-            break;
+            if (choice == 1) {
+                system("cls");
+                game.initializePlayers();
+                game.startGame();
+                break;
+            }
+            else if (choice == 2) {
+                system("cls");
+                game.executeCommand(new LoadGameCommand(&game));
+                std::cout << "Game loaded." << std::endl;
+                break;
+            }
+            else if (choice == 3) {
+                std::cout << "Goodbye!" << std::endl;
+                return 0;
+            }
+            else {
+                std::cout << "Invalid option. Try again." << std::endl;
+            }
         }
-        else if (choice == 2) {
-            system("cls");
-            game.executeCommand(new LoadGameCommand(&game));
-            std::cout << "Game loaded." << std::endl;
-            break;
-        }
-        else if (choice == 3) {
-            std::cout << "Goodbye!" << std::endl;
-            return 0;
-        }
-        else {
-            std::cout << "Invalid option. Try again." << std::endl;
-        }
-    }
 
-    while (!game.isGameOver()) {
-        game.nextTurn();
+        while (!game.isGameOver()) {
+            game.nextTurn();
+        }
+        game.announceWinner();
     }
-    game.announceWinner();
+    catch (const std::exception& ex) {
+        std::cerr << "An error occurred: " << ex.what() << std::endl;
+        return -1;
+    }
     return 0;
 }
